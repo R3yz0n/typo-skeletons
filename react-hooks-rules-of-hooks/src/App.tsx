75138e2ffe6ce78c0eca34d862e9450920e33b31
Sagar Thapa Chhetri <111417232+R3yz0n@users.noreply.github.com>
2025-01-05 17:22:43 +0545

    Feat/show runtime linting (#1)
    * add configuration and readme file
    
    * update README to include vite-plugin-eslint for improved React Hooks linting
    
    * fix: correct HTML syntax in README for proper rendering
    
    * fix: correct HTML syntax in README for proper rendering
    
    * fix: correct HTML syntax in README for proper rendering

--- react-hooks-rules-of-hooks/src/App.tsx
+++ react-hooks-rules-of-hooks/src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = React.useState(0);
+
+  const increment = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+  useEffect(() => {
+    increment();
+  }, [increment]);
+
+  return (
+    <div style={{ padding: 12 }}>
+      <br />
+      <br />
+      <h3> This app uses the "eslint-plugin-react-hooks" package as a dev dependencies.</h3>
+      <br />
+      In the vite.config.ts file, the eslint plugin is added to the plugins array. <br /> On doing
+      so the dev server would show the errors and warnings in the run time. <br /> Thats why the
+      errors like unused vars would be treated as error so to fix it use this :- <br />
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    </div>
+  );
 };
 
 export default App;
